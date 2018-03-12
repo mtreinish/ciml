Fix length normalization and add vectorization
This commit fixes the previously added length normalization. In my
testing using append() on a dataframe or a series didn't do what I
expected initially. At the same time this also adds vectorization to the
dataframe. So instead of a big 5500x31 dataframe it returns 2 vectors of
170500 length. The first is all the values and the second is the labels
of each type of data (just in case we need it)

ciml/trainer.py
@@ -33,14 +33,22 @@ def normalize_data(result, normalized_length=5500):
 
     # Fix length of dataset
     init_len = len(result)
+    dstat_keys = result.keys()
+
     if init_len > normalized_length:
         result = result[:normalized_length]
     elif init_len < normalized_length:
-        dstat_keys = result.keys()
         pad_length = normalized_length - init_len
         padd = pd.DataFrame(0, index=np.arange(pad_length), columns=dstat_keys)
-        result.append(padd)
-    return result
+        result = pd.concat([result, padd])
+    # Vectorize the dataframe
+    vector = pd.Series()
+    labels = pd.Series()
+    for key in dstat_keys:
+        vector = pd.concat([vector, result[key]])
+        labels = pd.concat(
+            [labels, pd.Series(key, index=np.arange(normalized_length))])
+    return vector, labels
 
 
 def train_results(results, model):