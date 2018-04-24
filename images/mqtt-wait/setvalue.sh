571df808e5f6142a9649e240bb82087d7af76d44
Matthew Treinish <mtreinish@kortar.org>
2018-04-24 14:39:37 -0400

    Add docker images for building a pipeline
    This commit starts building out the docker images needed for setting up
    the CIML pipeline. Not everything is there yet, like we'll probably need
    a separate image for the training batch jobs (or mqtt-trainer) but this
    starts the expansion.

---
+++ images/mqtt-wait/setvalue.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+for i in {1..300}; do
+    MQTT_HOST=$(kubectl get svc ${MQTT_CONTAINER_NAME} -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    if [[ -z "$MQTT_HOST" ]]; then
+        sleep 2
+    else
+        kubectl create secret generic ${MQTT_SECRET_NAME} --from-literal=host=${MQTT_HOST}
+        exit 0
+    fi
+done
+
+exit 1
