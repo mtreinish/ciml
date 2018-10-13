66504e4784f695e081f79bc58ee076a75bf95b83
Andrea Frittoli <andrea.frittoli@gmail.com>
2018-10-13 12:15:30 +0100

    Use a custom docker image for training
    Use a custom docker image for training which comes with git
    installed as well as all CIML dependencies, so we don't need
    to install everything before each training.

--- ffdl/train.sh
+++ ffdl/train.sh
@@ -6,15 +6,11 @@ export LANG=C.UTF-8
 DATASET=${1:-DATASET}
 EXPERIMENT=${2:-EXPERIMENT}
 
-# Install any missing depenency
-grep -v tensorflow requirements.txt > notf_requirements.txt
-pip install -r notf_requirements.txt
-apt-get update
-apt-get install -y python3.5-tk
+# Install CIML
+pip install .
 
 # Run the training
-python3 -c "from ciml.trainer import local_trainer; local_trainer()" \
-  --data-path $DATA_DIR --dataset $DATASET --experiment $EXPERIMENT --gpu
+ciml-train-model --data-path $DATA_DIR --dataset $DATASET --experiment $EXPERIMENT --gpu
 
 # Save the training results
 cp -r $MODEL_DIR/data $RESULT_DIR/
