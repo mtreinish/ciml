05dd6ac0db7144569f6a0006f8a7d04973c361bf
Andrea Frittoli <andrea.frittoli@gmail.com>
2018-10-18 10:00:51 +0100

    Make the ffdl_train script usable from out of tree

--- ffdl_train.sh
+++ ffdl_train.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 # Usage:
-#   export S3_AUTH_URL S3_ACCESS_KEY_ID S3_SECRET_ACCESS_KEY
+#   export S3_AUTH_URL AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
 #   ffdl_train.sh dataset experiment
 
+CIML_DIR=$(cd $(dirname $0); pwd)
+RUN_DIR=$(pwd)
+
 # Generate the model zip
-git archive -o ffdl/ciml.zip HEAD
+pushd "$CIML_DIR"
+mkdir -p "${RUN_DIR}/ffdl"
+git archive -o "$RUN_DIR/ffdl/ciml.zip" HEAD
+popd
 
 # Number of GPUs to use
 GPUS=${3:-0}
 
 # Generate the manifest file
-python ffdl/make-manifest.py \
+python "$CIML_DIR/ffdl/make-manifest.py" \
   --experiment $2 --dataset $1 --gpus $GPUS \
-  --s3-access-key-id $S3_ACCESS_KEY_ID \
-  --s3-secret-access-key $S3_SECRET_ACCESS_KEY \
+  --s3-access-key-id $AWS_ACCESS_KEY_ID \
+  --s3-secret-access-key $AWS_SECRET_ACCESS_KEY \
   --s3-auth-url $S3_AUTH_URL > ffdl/manifest.yaml
 
 # Expects ffdl to be installed and in the PATH
